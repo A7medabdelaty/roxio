4b04692d3e160e1f8b46c0e93836672745957aa7
A7medabdelaty <elqahwagy.123@gmail.com>
2025-01-04 21:49:17 +0200

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:roxio/core/utils/app_routes.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const RoxioApp());
 }
 
